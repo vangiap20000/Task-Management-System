824feb627a9457f10524a795a0c63e9659ed9c07
GiapHV <giaphv.nta@gmail.com>
2025-07-31 15:16:34 +0700

    [Fix]: Fix conflit code #

--- src/main/java/com/example/taskmanager/validators/LabelIdsValidator.java
+++ src/main/java/com/example/taskmanager/validators/LabelIdsValidator.java
@@ -6,21 +6,21 @@ import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Component
-public class LabelIdsValidator implements ConstraintValidator<ValidLabelIds, Long[]> {
+public class LabelIdsValidator implements ConstraintValidator<ValidLabelIds, List<Integer>> {
 
     @Autowired
     private LabelRepository labelRepository;
 
     @Override
-    public boolean isValid(Long[] values, ConstraintValidatorContext context) {
-        if (values == null || values.length == 0) {
+    public boolean isValid(List<Integer> values, ConstraintValidatorContext context) {
+        if (values == null || values.size() == 0) {
             return true;
         }
 
-        return Arrays.stream(values)
+        return values.stream()
                 .allMatch(id -> labelRepository.existsById(id));
     }
 }
